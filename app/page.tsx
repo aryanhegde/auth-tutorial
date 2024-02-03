7ffc5882068262d9676d6f975a33050770a6cd6b
aryanhegde <hegdearyan000@gmail.com>
2024-02-03 11:56:31 +0530

    Card Wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button className="flex items-center justify-center  mx-auto text-gray-600 mt-10 shadow-md shadow-lg hover:shadow-foreground">
-      Hey There! Click Me
-    </Button>
+    <main className="flex justify-center items-center   h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to-blue-600">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl text-white font-bold drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
