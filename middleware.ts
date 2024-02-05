118fa38645b01230921352c25bafa72e6beeff38
aryanhegde <hegdearyan000@gmail.com>
2024-02-05 23:57:26 +0530

    Middleware and Login 2

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
-import { auth } from "./auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
   console.log("ROUTE: ", req.nextUrl.pathname);
   console.log("Is Logged In: ", isLoggedIn);
 });
 
-// Optionally, don't invoke Middleware on some paths
-// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
